d9342bace059d8123dd69d1d974e99e554fc8f0e
steven <steven.h@live.com>
2021-11-20 21:55:51 -0800

    add last name form

--- src/components/GeneralInformation/GeneralInformation.js
+++ src/components/GeneralInformation/GeneralInformation.js
@@ -7,6 +7,10 @@ export default class GeneralInformation extends React.Component {
         <label htmlFor="firstName">First name</label>
         <br />
         <input id="firstName" type="text" />
+        <br />
+        <label htmlFor="lastName">Last name</label>
+        <br />
+        <input id="lastName" type="text" />
       </form>
     );
   }
--- src/components/GeneralInformation/GeneralInformation.test.js
+++ src/components/GeneralInformation/GeneralInformation.test.js
@@ -7,4 +7,10 @@ describe("GeneralInformation", () => {
     const firstNameElement = screen.getByLabelText(/first name/i);
     expect(firstNameElement).toBeInTheDocument();
   });
+
+  it("should render last name form", () => {
+    render(<GeneralInformation />);
+    const lastNameElement = screen.getByLabelText(/last name/i);
+    expect(lastNameElement).toBeInTheDocument();
+  });
 });
