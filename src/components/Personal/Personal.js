d34a89ccfc7bba1ab2ac5ab41840c4d69c4136af
steven <steven.h@live.com>
2021-11-21 14:58:31 -0800

    satisfy tests

--- src/components/Personal/Personal.js
+++ src/components/Personal/Personal.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Input from "./Input/Input";
 import PersonalForm from "./PersonalForm/PersonalForm";
 
 export default class Personal extends React.Component {
@@ -7,6 +6,14 @@ export default class Personal extends React.Component {
     super(props);
     this.state = {
       formVisible: true,
+      personalInfo: {
+        firstName: "",
+        lastName: "",
+        addressLineOne: "",
+        city: "",
+        state: "",
+        postalCode: "",
+      },
     };
   }
 
@@ -14,11 +21,36 @@ export default class Personal extends React.Component {
     e.preventDefault();
     this.setState({
       formVisible: !this.state.formVisible,
+      personalInfo: {
+        firstName: e.target.elements[0].value,
+        lastName: e.target.elements[1].value,
+        addressLineOne: e.target.elements[2].value,
+        city: e.target.elements[4].value,
+        state: e.target.elements[5].value,
+        postalCode: e.target.elements[6].value,
+      },
     });
   };
 
   render() {
     let { formVisible } = this.state;
-    return <PersonalForm submit={this.handleSubmit} isVisible={formVisible} />;
+    let { firstName, lastName, addressLineOne, city, state, postalCode } =
+      this.state.personalInfo;
+    return (
+      <>
+        <PersonalForm submit={this.handleSubmit} isVisible={formVisible} />
+        <div className={!formVisible ? "" : "hidden"}>
+          <div>
+            {firstName} {lastName}
+          </div>
+          <div>
+            <div>{addressLineOne}</div>
+            <div>
+              {city}, {state} {postalCode}
+            </div>
+          </div>
+        </div>
+      </>
+    );
   }
 }
