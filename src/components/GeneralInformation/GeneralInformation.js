e94bb3477493550b2fd370fef4ed4b3d92b35acb
steven <steven.h@live.com>
2021-11-20 22:32:39 -0800

    add rest of general info and tests

--- src/components/GeneralInformation/GeneralInformation.js
+++ src/components/GeneralInformation/GeneralInformation.js
@@ -6,11 +6,35 @@ export default class GeneralInformation extends React.Component {
       <form>
         <label htmlFor="firstName">First name</label>
         <br />
-        <input id="firstName" type="text" />
+        <input type="text" id="firstName" />
         <br />
         <label htmlFor="lastName">Last name</label>
         <br />
-        <input id="lastName" type="text" />
+        <input type="text" id="lastName" />
+        <br />
+        <label htmlFor="addressLineOne">Address line 1</label>
+        <br />
+        <input type="text" id="addressLineOne" />
+        <br />
+        <label htmlFor="addressLineTwo">Address line 2</label>
+        <br />
+        <input type="text" id="addressLineTwo" />
+        <br />
+        <label htmlFor="city">City</label>
+        <br />
+        <input type="text" name="" id="city" />
+        <br />
+        <label htmlFor="postalCode">Postal code</label>
+        <br />
+        <input type="text" name="" id="postalCode" />
+        <br />
+        <label htmlFor="email">Email</label>
+        <br />
+        <input type="text" name="" id="email" />
+        <br />
+        <label htmlFor="phone">Phone</label>
+        <br />
+        <input type="text" name="" id="phone" />
       </form>
     );
   }
--- src/components/GeneralInformation/GeneralInformation.test.js
+++ src/components/GeneralInformation/GeneralInformation.test.js
@@ -1,16 +1,47 @@
 import { render, screen } from "@testing-library/react";
 import GeneralInformation from "./GeneralInformation";
 
+beforeEach(() => {
+  render(<GeneralInformation />);
+});
 describe("GeneralInformation", () => {
   it("should render first name", () => {
-    render(<GeneralInformation />);
     const firstNameElement = screen.getByLabelText(/first name/i);
     expect(firstNameElement).toBeInTheDocument();
   });
 
   it("should render last name form", () => {
-    render(<GeneralInformation />);
     const lastNameElement = screen.getByLabelText(/last name/i);
     expect(lastNameElement).toBeInTheDocument();
+  });
+
+  it("should render address line 1", () => {
+    const addressOneElement = screen.getByLabelText(/address line 1/i);
+    expect(addressOneElement).toBeInTheDocument();
+  });
+
+  it("should render address line 2", () => {
+    const addressTwoElement = screen.getByLabelText(/address line 2/i);
+    expect(addressTwoElement).toBeInTheDocument();
+  });
+
+  it("should render city", () => {
+    const cityElement = screen.getByLabelText(/city/i);
+    expect(cityElement).toBeInTheDocument();
+  });
+
+  it("should render postal code", () => {
+    const postalCode = screen.getByLabelText(/postal code/i);
+    expect(postalCode).toBeInTheDocument();
+  });
+
+  it("should render email", () => {
+    const emailElement = screen.getByLabelText(/email/i);
+    expect(emailElement).toBeInTheDocument();
+  });
+
+  it("should render phone", () => {
+    const phoneElement = screen.getByLabelText(/phone/i);
+    expect(phoneElement).toBeInTheDocument();
   });
 });
