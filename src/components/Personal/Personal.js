ed7cdc4cd80a834a9c1864d9f9789be2acc47649
steven <steven.h@live.com>
2021-11-21 01:30:19 -0800

    refactor form into PersonalForm

--- src/components/Personal/Personal.js
+++ src/components/Personal/Personal.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Input from "./Input/Input";
+import PersonalForm from "./PersonalForm/PersonalForm";
 
 export default class Personal extends React.Component {
   constructor(props) {
@@ -18,22 +19,6 @@ export default class Personal extends React.Component {
 
   render() {
     let { formVisible } = this.state;
-    return (
-      <form
-        aria-label="form"
-        onSubmit={this.handleSubmit}
-        className={formVisible ? "" : "hidden"}
-      >
-        <Input id="firstName" text="First name" />
-        <Input id="lastName" text="Last name" />
-        <Input id="addressLineOne" text="Address line 1" />
-        <Input id="addressLineTwo" text="Address line 2" />
-        <Input id="city" text="City" />
-        <Input id="postalCode" text="Postal code" />
-        <Input id="email" text="Email" />
-        <Input id="phone" text="Phone" />
-        <button type="submit">Submit</button>
-      </form>
-    );
+    return <PersonalForm submit={this.handleSubmit} isVisible={formVisible} />;
   }
 }
---
+++ src/components/Personal/PersonalForm/PersonalForm.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Input from "../Input/Input";
+
+export default class PersonalForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    let { submit, isVisible } = this.props;
+    return (
+      <form
+        aria-label="form"
+        onSubmit={submit}
+        className={isVisible ? "" : "hidden"}
+      >
+        <Input id="firstName" text="First name" />
+        <Input id="lastName" text="Last name" />
+        <Input id="addressLineOne" text="Address line 1" />
+        <Input id="addressLineTwo" text="Address line 2" />
+        <Input id="city" text="City" />
+        <Input id="postalCode" text="Postal code" />
+        <Input id="email" text="Email" />
+        <Input id="phone" text="Phone" />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
