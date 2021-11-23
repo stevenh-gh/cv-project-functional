1e3d38f93960e268571caee2f98b64180c0fd654
steven <steven.h@live.com>
2021-11-22 21:32:16 -0800

    add test for edit button

--- src/components/Personal/PersonalInfo/PersonalInfo.test.js
+++ src/components/Personal/PersonalInfo/PersonalInfo.test.js
@@ -21,4 +21,18 @@ describe("PersonalInfo", () => {
     const editButtonElement = screen.getByTestId("editbtn");
     expect(editButtonElement).toBeInTheDocument();
   });
+
+  it("should change cursor to pointer when hover over edit button", () => {
+    const editButtonElement = screen.getByTestId("editbtn");
+    expect(editButtonElement).toHaveClass("cursor-pointer");
+  });
+
+  // this works, but unsure how to test
+  // a method call that is passed as a prop when clicked
+  it.skip("should call edit method when clicked", () => {
+    const editButtonElement = screen.getByTestId("editbtn");
+    const spy = jest.spyOn(editButtonElement, "edit");
+    fireEvent.click(editButtonElement);
+    expect(spy).toBeCalled();
+  });
 });
