edbee9ad7a37710c816c73029b4bca7b68726758
steven <steven.h@live.com>
2021-11-23 15:15:22 -0800

    satisfy the tests

--- src/components/Personal/Input/Input.js
+++ src/components/Personal/Input/Input.js
@@ -2,12 +2,12 @@ import React from "react";
 
 export default class Input extends React.Component {
   render() {
-    const { id, text } = this.props;
+    const { id, text, val } = this.props;
     return (
       <>
         <label htmlFor={id}>{text}</label>
         <br />
-        <input type="text" name="" id={id} />
+        <input type="text" name="" id={id} value={val} />
         <br />
       </>
     );
