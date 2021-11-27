f66e285c86a22307846b920c4512b6ef56993623
steven <steven.h@live.com>
2021-11-26 23:25:18 -0800

    change educationform to use input from src level component and use education props

--- src/components/Education/EducationForm/EducationForm.js
+++ src/components/Education/EducationForm/EducationForm.js
@@ -1,26 +1,21 @@
 import React from "react";
-import Input from "../Input/Input";
+import Input from "../../Input/Input";
 
-export default class PersonalForm extends React.Component {
+export default class EducationForm extends React.Component {
   render() {
     const { submit, isVisible, info } = this.props;
-    const { firstName } = info.firstName;
+    const { institution } = info.institution;
     return (
       <form
         aria-label="form"
         onSubmit={submit}
         className={isVisible ? "" : "hidden"}
       >
-        <Input id="firstName" text="First name" val={firstName} />
-        <Input id="lastName" text="Last name" />
-        <Input id="addressLineOne" text="Address line 1" />
-        <Input id="addressLineTwo" text="Address line 2" />
-        <Input id="city" text="City" />
-        <Input id="state" text="State" />
-        <Input id="postalCode" text="Postal code" />
-        <Input id="email" text="Email" />
-        <Input id="phone" text="Phone" />
-        <button type="submit">Submit</button>
+        <Input id="institution" text="Name of institution" val={institution} />
+        <Input id="fromYear" text="From year" />
+        <Input id="toYear" text="To year" />
+        <Input id="degree" text="Degree" />
+       <button type="submit">Submit</button>
       </form>
     );
   }
