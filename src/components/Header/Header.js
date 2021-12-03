8cb4c41ede29c972a016871e3a7fc5e6ee58d118
steven <steven.h@live.com>
2021-12-03 14:08:02 -0800

    style header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default class Header extends React.Component {
   render() {
-    return <h1 className="text-center">Odin CV Application</h1>;
+    return <h1 className="text-center mb-7 text-3xl">Odin CV Application</h1>;
   }
 }
