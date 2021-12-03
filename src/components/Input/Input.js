d828c35765027cd86cc8de1b2193c482eada6e32
steven <steven.h@live.com>
2021-12-02 22:24:37 -0800

    make input width 100%

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -5,20 +5,22 @@ export default class Input extends React.Component {
     const { id, text, val } = this.props;
     return (
       <>
-        <label
-          className="block uppercase tracking-wide  text-gray-700 text-xs font-bold mb-2"
-          htmlFor={id}
-        >
-          {text}
-        </label>
-        <br />
-        <input
-          className="appearance-none bg-gray-200 text-gray-700 border border-gray-200 rounded focus:outline-none focus:bg-white focus:border-gray-500 leading-tight mb-3 py-3 px-4"
-          type="text"
-          name=""
-          id={id}
-          value={val}
-        />
+        <div>
+          <label
+            className="block uppercase tracking-wide  text-gray-700 text-xs font-bold mb-2"
+            htmlFor={id}
+          >
+            {text}
+          </label>
+          <br />
+          <input
+            className="appearance-none bg-gray-200 text-gray-700 border border-gray-200 rounded focus:outline-none focus:bg-white focus:border-gray-500 leading-tight mb-3 py-3 px-4 w-full"
+            type="text"
+            name=""
+            id={id}
+            value={val}
+          />
+        </div>
         <br />
       </>
     );
