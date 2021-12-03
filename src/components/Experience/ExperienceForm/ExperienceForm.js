e9cb84bfd1132fff36d7ac725fe9ba92ab10e8e3
steven <steven.h@live.com>
2021-12-03 14:32:43 -0800

    style experienceform

--- src/components/Experience/ExperienceForm/ExperienceForm.js
+++ src/components/Experience/ExperienceForm/ExperienceForm.js
@@ -11,12 +11,29 @@ export default class ExperienceForm extends React.Component {
         onSubmit={submit}
         className={isVisible ? "" : "hidden"}
       >
-        <Input id="company" text="Company" val={company} />
-        <Input id="position" text="Position at company" />
-        <Input id="responsiblities" text="Responsibilities" />
-        <Input id="fromYear" text="From year" />
-        <Input id="toYear" text="To year" />
-        <button type="submit">Submit</button>
+        <div className="grid grid-cols-12 gap-2 mt-4">
+          <div className="col-span-12">
+            <Input id="company" text="Company" val={company} />
+          </div>
+          <div className="col-span-6">
+            <Input id="position" text="Position at company" />
+          </div>
+          <div className="col-span-6">
+            <Input id="responsiblities" text="Responsibilities" />
+          </div>
+          <div className="col-span-6">
+            <Input id="fromYear" text="From year" />
+          </div>
+          <div className="col-span-6">
+            <Input id="toYear" text="To year" />
+          </div>
+          <button
+            className="col-span-2 col-start-6 py-1 bg-green-400 border-green-400 hover:border-green-500 hover:bg-green-500 text-sm border-4 text-white rounded mb-4"
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
       </form>
     );
   }
