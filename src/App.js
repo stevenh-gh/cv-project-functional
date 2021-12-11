2004e039e43128f02d8aa46a400de77c8b7f788e
steven <steven.h@live.com>
2021-12-10 17:20:19 -0800

    convert app to function component

--- src/App.js
+++ src/App.js
@@ -4,21 +4,19 @@ import Experience from "./components/Experience/Experience";
 import Header from "./components/Header/Header";
 import Personal from "./components/Personal/Personal";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="container mx-auto grid grid-cols-12">
-        <div className="col-span-6 col-start-4">
-          <Header />
-          <Personal />
-          <hr />
-          <Education />
-          <hr />
-          <Experience />
-        </div>
+const App = () => {
+  return (
+    <div className="container mx-auto grid grid-cols-12">
+      <div className="col-span-6 col-start-4">
+        <Header />
+        <Personal />
+        <hr />
+        <Education />
+        <hr />
+        <Experience />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
