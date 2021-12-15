f78509dc6abacbd712b664bb88d2ab288367a4ca
steven <steven.h@live.com>
2021-12-15 15:05:14 -0800

    convert experience.js into function component

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,57 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import ExperienceForm from "./ExperienceForm/ExperienceForm";
 import ExperienceInfo from "./ExperienceInfo/ExperienceInfo";
 
-export default class Experience extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      formVisible: true,
-      experienceInfo: {
-        company: "",
-        position: "",
-        responsibilities: "",
-        fromYear: "",
-        toYear: "",
-      },
-    };
-  }
-
-  handleSubmit = (e) => {
+const Experience = () => {
+  const [formVisible, setFormVisible] = useState(true);
+  const [experienceInfo, setExperienceInfo] = useState({
+    company: "",
+    position: "",
+    responsibilities: "",
+    fromYear: "",
+    toYear: "",
+  });
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.setState({
-      formVisible: !this.state.formVisible,
-      experienceInfo: {
-        company: e.target.elements[0].value,
-        position: e.target.elements[1].value,
-        responsibilities: e.target.elements[2].value,
-        fromYear: e.target.elements[3].value,
-        toYear: e.target.elements[4].value,
-      },
+    setFormVisible(!formVisible);
+    setExperienceInfo({
+      company: e.target.elements[0].value,
+      position: e.target.elements[1].value,
+      responsibilities: e.target.elements[2].value,
+      fromYear: e.target.elements[3].value,
+      toYear: e.target.elements[4].value,
     });
   };
 
-  edit = () => {
+  const edit = () => {
     // console.log("working");
-    this.setState({
-      formVisible: !this.state.formVisible,
-    });
+    setFormVisible(!formVisible);
   };
 
-  render() {
-    return (
-      <>
-        <ExperienceForm
-          submit={this.handleSubmit}
-          isVisible={this.state.formVisible}
-          info={this.state.experienceInfo}
-        />
-        <ExperienceInfo
-          eInfo={this.state.experienceInfo}
-          visibility={this.state.formVisible}
-          edit={this.edit}
-        />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <ExperienceForm
+        submit={handleSubmit}
+        isVisible={formVisible}
+        info={experienceInfo}
+      />
+      <ExperienceInfo
+        eInfo={experienceInfo}
+        visibility={formVisible}
+        edit={edit}
+      />
+    </>
+  );
+};
+export default Experience;
